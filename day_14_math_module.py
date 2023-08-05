3a5b34711beeeae78e5483e31f027ce4789a264b
V CHANDRA SHEKAR REDDY <113517229+ChanduReddy999@users.noreply.github.com>
2023-08-05 21:26:45 +0530

    Add files via upload

---
+++ day_14_math_module.py
@@ -0,0 +1,96 @@
+''' Day 14 Math module'''
+
+import math
+# print(math.ceil(0.1))
+# print(math.ceil(10.9))
+# print(math.ceil(220.5))
+# print(math.ceil(0.45))
+# print(math.ceil(-0.499))
+# print(math.ceil(14.1))
+# print(math.ceil(-10.1))
+
+# print(math.exp(1))          # 2.718281828459045
+# print(math.exp(3))          # 20.085536923187668
+# print(math.exp(10))         # 22026.465794806718
+# print(math.exp(10.7))       # 44355.85513029784 
+# print(math.exp(11))         # 59874.14171519782 
+
+# print(math.factorial(0))
+# print(math.factorial(1))
+# print(math.factorial(2))
+# print(math.factorial(3))
+# print(math.factorial(5))
+# print(math.factorial(10))
+
+# print(math.floor(0.09))
+# print(math.floor(1.02))
+# print(math.floor(-89.234))
+# print(math.floor(29))
+# print(math.floor(9.99999))
+# print(math.floor(78.97))
+
+
+'''when math module is used we get answers in terms of float in calculations'''
+# a=math.pow(8,2)
+# print(a,type(a))            # 64.0 float math module is used in the calculations
+# b=3**3
+# print(b,type(b))        # 27 int    no math module is used in this.
+
+# print(math.pi)
+# print(math.pi*2)
+# print(math.pi*2**3)
+
+# print(math.sqrt(36))
+# print(math.sqrt(28))
+# print(math.sqrt(96.945))
+# print(math.sqrt(-96.945))      # ValueError: math domain error
+
+# c=2          # 1 to 3 will give 1 if used isqrt
+# d=6          # 4 to 8 will give 2 if used isqrt
+# e=14         # 9 to 15 will give 3 if used isqrt
+# f=0          # 0 will give 0 if used isqrt
+# print(math.isqrt(c))        # Return the integer part of the square root of the input.
+# print(math.isqrt(d))
+# print(math.isqrt(e))
+# print(math.isqrt(f))
+
+# n=[1,2,3,4,5]
+# print(sum(n))
+# print(math.fsum(n))
+# print(math.prod(n))
+# print(int(math.fsum(n)))
+
+# g=17%3
+# print(g)
+# print(math.fmod(17,4))
+# print(int(math.fmod(17,4)))
+
+# v=-5
+# print(abs(v))
+# a=abs(int(input()))
+# print("the person age is:",a)
+
+# print(math.fabs(-8))
+# print(math.cos(45))
+# print(math.sin(45))
+# print(math.tan(45))
+# print(math.trunc(4.55))         # 4    Truncates the Real x to the nearest Integral toward 0.
+# print(math.trunc(-74.5))        # -74  Truncates the Real x to the nearest Integral toward 0.
+
+# print(int(-6.55))
+
+# print(math.gcd(13,39))
+# print(math.gcd(12,28))
+# print(math.gcd(131,39))
+# print(math.gcd(12,212))
+# print(math.gcd(12,50))
+# print(math.gcd(12,-24))
+# print(math.gcd(-12,-240))
+
+# print(math.lcm(12,28))
+# print(math.lcm(6,18))
+
+
+# import math
+
+# print(dir(math))
